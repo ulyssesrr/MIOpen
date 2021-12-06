77b82876c4162c5947ab217b204546b4f5a05e1c
shaojiewang <wsjmessi@163.com>
2021-12-06 11:19:22 +0800

    [skip ci] add 128 alignment for ocl be

--- src/solver/conv_asm_implicit_gemm_gtc_fwd_nhwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_fwd_nhwc.cpp
@@ -774,17 +774,17 @@ ConvAsmImplicitGemmGTCDynamicFwdXdlopsNHWC::GetWorkspaceSize(const ConvolutionCo
         if(!trans_input.IsSkippable())
         {
             workspace_size += trans_input.GetSize();
-            workspace_size = ((workspace_size + 31) >> 5) << 5;
+            workspace_size = ((workspace_size + 127) >> 7) << 7;
         }
         if(!trans_weight.IsSkippable())
         {
             workspace_size += trans_weight.GetSize();
-            workspace_size = ((workspace_size + 31) >> 5) << 5;
+            workspace_size = ((workspace_size + 127) >> 7) << 7;
         }
         if(!trans_output.IsSkippable())
         {
             workspace_size += trans_output.GetSize();
-            workspace_size = ((workspace_size + 31) >> 5) << 5;
+            workspace_size = ((workspace_size + 127) >> 7) << 7;
         }
 
         // 4 bytes alignment to do atomic add
